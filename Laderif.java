7452aff68543c7af28f47d9fa9dd939933ce2514
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2022-03-09 20:11:56 +0530

    Add files via upload

---
+++ Laderif.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Laderif
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int a;
+System.out.println("Enter any number:");
+a=sc.nextInt();
+
+  if(a!=0)
+    {
+      if(a>0)
+       {
+         if(a%2==0)
+          {
+          System.out.println("Given number is Even");
+}
+}
+}
+}
+}
